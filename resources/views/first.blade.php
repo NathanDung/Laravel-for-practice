2248b4c440a635d46036cd3dad22e5c794a3c453
NathanD <illuminator1520@gmail.com>
2018-12-24 12:49:30 +0800

    Finished Laravel 5.7 Episode -5 and make 3 types of return, pretty interesting !

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -5,5 +5,22 @@
 @endsection
 
 @section('content')
-	<h1>My First Websites!</h1>
-@endsection('content') 
+
+	<!-- <h1>My <?= $foo; ?> Websites!</h1> -->
+	<!-- old type of PHP -->
+	<h1>My {{ $foo }} Websites!</h1>
+	<!-- using Laravel blade in new type of PHP -->
+
+<!-- 	<ul>
+		<?php foreach($tasks as $task) : ?>
+			<li><?= $task; ?></li>
+		<?php endforeach; ?>	
+	</ul> --> 
+	<!-- old type of PHP -->
+
+	@foreach($tasks as $task)
+		<li>{{ $task }}</li>
+	@endforeach 
+	<!-- using Laravel blade in new type of PHP -->
+
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,42 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
+
     return view('welcome');
 });
 
-// Route::get('/', function () {
-//     return view('first');
-// });//Laravel影片-4
+Route::get('/', function () {
+	return view('first',[
+		'tasks' => [
+			'Go to the store',
+			'Go to the market',
+			'Go to work',
+			'Go to the concert'
+		],
+		'foo' => request('inputbyyourself')
+	]); //Laravel video episode -5
+
+	/* $tasks = [
+	 	'Go to the store',
+	 	'Go to the market',
+	 	'Go to work',
+	 	'Go to the concert'
+	 ];
+
+	 return view('first')->withtasks($tasks)->withfoo('Home');
+	*/
+
+	/*return view('first', [
+    	'tasks' => $tasks,
+    	'foo' => 'Home'
+    	//Call foo in first.blade.php page can get 'Home' this result.
+
+    	//'foo' => request('title')
+    	//This array can be also like 'foo' => 'foobar', and when uses foo in first.blade.php view, it can returns "foobar".
+    ]); */
+
+});//Laravel影片-4
 
 Route::get('/second', function () {
 	return view('second');
